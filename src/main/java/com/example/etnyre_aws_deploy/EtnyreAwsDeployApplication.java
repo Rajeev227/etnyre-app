49247454d0e02040b5b88c9227e979472449495b
rahul <rahulutturi3256@gmail.com>
2024-10-01 13:08:51 +0530

    first commit

---
+++ src/main/java/com/example/etnyre_aws_deploy/EtnyreAwsDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.etnyre_aws_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtnyreAwsDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtnyreAwsDeployApplication.class, args);
+	}
+
+}
